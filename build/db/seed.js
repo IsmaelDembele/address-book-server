3590635859d41d509e0a715f4de345498f6c8224
ismaelDembele <dembele.ismael@gmail.com>
2021-10-06 14:02:09 -0400

    add a Procfile

--- build/db/seed.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const utils_1 = require("../src/utils");
-(0, utils_1.createUserTable)();
-(0, utils_1.createContactTable)();
