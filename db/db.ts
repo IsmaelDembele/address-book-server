7995cdb1fdd29ccf0133b082105f22e32e125ce1
ismaelDembele <dembele.ismael@gmail.com>
2021-10-06 10:59:17 -0400

    database url updated to production

--- db/db.ts
+++ db/db.ts
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.DB_USER);
+const connectionString = process.env.DATABASE_URL;
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: +process.env.DB_PORT!,
-});
+export const pool =
+  process.env.NODE_ENV === "production"
+    ? new Pool({ connectionString })
+    : new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: +process.env.DB_PORT!,
+      });
