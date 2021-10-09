8a4cbf5d845b0b519437bc298478f63712fb0398
ismaelDembele <dembele.ismael@gmail.com>
2021-10-09 08:23:45 -0400

    test database

--- db/db.ts
+++ db/db.ts
@@ -3,6 +3,13 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//       rejectUnauthorized: false
+//   }
+// });
+
 console.log(`
   user: ${process.env.DB_USER},
   host: ${process.env.DB_HOST},
@@ -11,10 +18,18 @@ console.log(`
   port: 5432,
 `);
 
-export const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
-  port: 5432,
-});
+export const pool =
+  process.env.NODE_ENV === "production"
+    ? new Pool({
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      })
+    : new Pool({
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS,
+        port: 5432,
+      });
